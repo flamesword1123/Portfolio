72eb9f28968499512696bb0eb8f905efdc96d4c4
Jacob Bernheim <bernheimjacob@gmail.com>
2017-12-31 23:10:21 -0500

    create navbar

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.resolve('./public')));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.resolve('./index.html'));
+    res.sendFile(path.resolve('./public/index.html'));
 });
+
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
